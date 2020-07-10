22de814361fb26ee9486adde805a200436a10c62
Jim Liu <jim.liu@berkeley.edu>
2020-07-10 03:46:24 -0400

    Fix linting warnings and add clarifying text to selectedCards

--- src/NoDropdownSelector/NoDropdownSelector.js
+++ src/NoDropdownSelector/NoDropdownSelector.js
@@ -4,7 +4,7 @@ import classes from './NoDropdownSelector.module.css';
 const Selector = (props) => {
 
     let buttons = props.collection.map(item => {
-        if (item == props.currentSelected) {
+        if (item === props.currentSelected) {
             return <button key={item} className={[classes.Button, classes.Missing].join(' ')}>{item}</button>
         } else {
             return <button key={item} className={classes.Button} onClick={() => props.changed(item)}>{item}</button>
--- src/SelectedCards/SelectedCards.js
+++ src/SelectedCards/SelectedCards.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { InputNumber } from 'antd';
 import classes from './SelectedCards.module.css';
-import minions from '../minions';
 
 const SelectedCards = (props) => {
 
@@ -31,12 +30,13 @@ const SelectedCards = (props) => {
 
         return (
             <div key={key}>
+                <span> at least &nbsp; </span>
                 {cardAmountInput}
                 &nbsp; {key}
                 <span> when </span>
                 {takenAmountInput}
                 &nbsp; <span>are taken already</span> &nbsp;
-                <img className={classes.trashcan} src={require('../assets/trash.svg')} onClick={() => props.delete(key)}/>
+                <img className={classes.trashcan} src={require('../assets/trash.svg')} alt="Remove" onClick={() => props.delete(key)}/>
             </div>
         )});
 
