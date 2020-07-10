96a99a6d180765cbf3443a17c6f9e7a0096e99b6
Jim Liu <jim.liu@berkeley.edu>
2020-07-10 02:18:10 -0400

    Add specifying text. Add dummy values for min, max of taken cards.

--- src/SelectedCards/SelectedCards.js
+++ src/SelectedCards/SelectedCards.js
@@ -4,19 +4,36 @@ import classes from './SelectedCards.module.css';
 import minions from '../minions';
 
 const SelectedCards = (props) => {
+
+    const header = <div>
+        What are the odds to hit
+    </div>
+
     const entries = Object.keys(props.selectedCards).map(key => {
         return (
-        <div key={key}>
-            {key}: 
-            <InputNumber min={0}
-                         max={props.tierCardCounts[props.minionsMap[key].Tier]}
-                         defaultValue={props.selectedCards[key]}
-                         onChange={(value) => props.changeCard(key, value)} />
-            <img className={classes.trashcan} src={require('../assets/trash.svg')} onClick={() => props.delete(key)}/>
-        </div>
-    )});
+            <div key={key}>
+                <InputNumber 
+                            min={0}
+                            max={props.tierCardCounts[props.minionsMap[key].Tier]}
+                            style={{width: '60px'}}
+                            defaultValue={props.selectedCards[key]}
+                            onChange={(value) => props.changeCard(key, value)} />
+                &nbsp; {key}
+                <span> when </span>
+                <InputNumber 
+                            min={0}
+                            max={5}
+                            style={{width: '60px'}}
+                            defaultValue={0} />
+                &nbsp; <span>are taken already</span> &nbsp;
+                <img className={classes.trashcan} src={require('../assets/trash.svg')} onClick={() => props.delete(key)}/>
+            </div>
+        )});
 
-    return entries
+    return <div>
+        {header}
+        {entries}
+    </div>
 }
 
 export default SelectedCards;
