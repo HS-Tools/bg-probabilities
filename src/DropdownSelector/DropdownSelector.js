c76e9625d989026d99e82555b4bb3624ef3585f2
Jim Liu <jim.liu@berkeley.edu>
2020-08-27 15:33:25 -0400

    Make the dropdown select autofocus

--- src/DropdownSelector/DropdownSelector.js
+++ src/DropdownSelector/DropdownSelector.js
@@ -13,10 +13,12 @@ class Selector extends Component {
             <div>
                 <span>What is the probability to see and buy these minions in the tavern? &nbsp; </span>
                 <Select 
+                    id="Select"
                     showSearch
                     style={{width: '250px'}}
                     placeholder='Add a minion'
                     value={null}
+                    autoFocus={true}
                     onChange={this.props.changed}>
                     {names}
                 </Select>
