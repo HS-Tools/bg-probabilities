9037c1e1ea7862d633db0bb9d70adf9aadb29e90
JimLiu0 <jim.liu@berkeley.edu>
2021-03-08 09:17:33 -0500

    Change css in DropdownSelector.module.css to reference a class and make img that class in the js file to prevent changing the css of other images

--- src/DropdownSelector/DropdownSelector.js
+++ src/DropdownSelector/DropdownSelector.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { Select } from 'antd'
 import cards from '../assets/cards.json';
 import './DropdownSelector.module.css';
+import classes from './DropdownSelector.module.css';
 const { Option } = Select;
 
 class Selector extends Component {
     render() {
         let names = this.props.collection.map(item => {
             const card = cards.find(card => card['name'] === item.Name);
-            const image = card && (<img src={`https://art.hearthstonejson.com/v1/orig/${card.id}.png`} alt={item.Name}/>);
+            const image = card && (<img className={classes.Clip} src={`https://art.hearthstonejson.com/v1/orig/${card.id}.png`} alt={item.Name}/>);
 
             return <Option key={item.Name} disabled={Object.keys(this.props.selectedCards).indexOf(item.Name) >= 0} value={item.Name}>
                 {image}
