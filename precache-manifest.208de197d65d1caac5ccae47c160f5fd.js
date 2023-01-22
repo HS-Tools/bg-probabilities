d430adaf54576a6bb4ee72ba2e7b67e49d371ba0
JimLiu0 <jim.liu.95@gmail.com>
2023-01-22 07:58:14 +0000

    Deploying to gh-pages from @ HS-Tools/bg-probabilities@83e49945991221a9e95eafb78c14f0503965fb26 🚀

--- precache-manifest.208de197d65d1caac5ccae47c160f5fd.js
+++ precache-manifest.208de197d65d1caac5ccae47c160f5fd.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d0eba6eaf8507b2c256eaae221158ab6",
+    "revision": "ead9362784bb3f4cfa17f6207a70118c",
     "url": "/bg-probabilities/index.html"
   },
   {
-    "revision": "684ce8bc4218623d5726",
+    "revision": "ad9378234f3f8278d100",
     "url": "/bg-probabilities/static/css/2.f9aa6948.chunk.css"
   },
   {
-    "revision": "66dd923d29b1484334cb",
+    "revision": "7b07172395c12b5d7731",
     "url": "/bg-probabilities/static/css/main.819343d8.chunk.css"
   },
   {
-    "revision": "684ce8bc4218623d5726",
-    "url": "/bg-probabilities/static/js/2.50ef263d.chunk.js"
+    "revision": "ad9378234f3f8278d100",
+    "url": "/bg-probabilities/static/js/2.f16e1fdc.chunk.js"
   },
   {
     "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
-    "url": "/bg-probabilities/static/js/2.50ef263d.chunk.js.LICENSE.txt"
+    "url": "/bg-probabilities/static/js/2.f16e1fdc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "66dd923d29b1484334cb",
-    "url": "/bg-probabilities/static/js/main.86af1174.chunk.js"
+    "revision": "7b07172395c12b5d7731",
+    "url": "/bg-probabilities/static/js/main.0d72911e.chunk.js"
   },
   {
     "revision": "2b72f798ea9b5991239a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bg-probabilities/precache-manifest.11abaaf9a8b26d69e23bff639d17e11a.js"
+  "/bg-probabilities/precache-manifest.208de197d65d1caac5ccae47c160f5fd.js"
 );
 
 self.addEventListener('message', (event) => {
